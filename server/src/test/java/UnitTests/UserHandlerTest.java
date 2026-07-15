e3a9fa60aefd215c93a988986499a3f03bd83d32
kodym <kodymckaywastlund@outlook.com>
2026-07-15 17:26:30 -0600

    Created 2 Test classes. StandardAPITests still passing.

---
+++ server/src/test/java/UnitTests/UserHandlerTest.java
@@ -0,0 +1,46 @@
+package UnitTests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserHandlerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void handle_register() {
+    }
+
+    @Test
+    void handle_clear() {
+    }
+
+    @Test
+    void handle_login() {
+    }
+
+    @Test
+    void handle_logout() {
+    }
+
+    @Test
+    void handle_list_games() {
+    }
+
+    @Test
+    void handle_create_game() {
+    }
+
+    @Test
+    void handle_join_game() {
+    }
+}
---
+++ server/src/test/java/UnitTests/UserServiceTest.java
@@ -0,0 +1,46 @@
+package UnitTests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void logout() {
+    }
+
+    @Test
+    void createGame() {
+    }
+
+    @Test
+    void listGames() {
+    }
+
+    @Test
+    void joinGame() {
+    }
+
+    @Test
+    void clear() {
+    }
+}
