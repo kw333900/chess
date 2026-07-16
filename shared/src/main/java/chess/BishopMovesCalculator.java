6b73efdfbfa13f7be0dff4da37b54bd9afa25f8a
kodym <kodymckaywastlund@outlook.com>
2026-07-15 20:33:44 -0600

    fixing code quality (fixed bishop movement)

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -12,25 +12,25 @@ public class BishopMovesCalculator {
 
 
     public Collection<ChessMove> calculateBishopMoves (ChessBoard board, ChessPosition myPosition, ChessPiece piece){
-        List<ChessMove> bishop_valid_moves = new ArrayList<>();
+        List<ChessMove> bishopValidMoves = new ArrayList<>();
         // validate (make sure it's on the board) the myPosition given as parameter
         if (myPosition.getRow() < 1 || myPosition.getRow() > 8 || myPosition.getColumn() < 1 || myPosition.getColumn() > 8) {
             return List.of();
         }
 
         // UP_RIGHT direction (row+1, col+1):
-        bishopDirection(board, myPosition, piece, 1, 1, bishop_valid_moves);
+        bishopDirection(board, myPosition, piece, 1, 1, bishopValidMoves);
 
         // DOWN_RIGHT direction (row-1, col+1):
-        bishopDirection(board, myPosition, piece, -1, 1, bishop_valid_moves);
+        bishopDirection(board, myPosition, piece, -1, 1, bishopValidMoves);
 
         // DOWN_LEFT direction (row-1, col-1):
-        bishopDirection(board, myPosition, piece, -1, -1, bishop_valid_moves);
+        bishopDirection(board, myPosition, piece, -1, -1, bishopValidMoves);
 
         // UP_LEFT direction (row+1, col-1):
-        bishopDirection(board, myPosition, piece, 1, -1, bishop_valid_moves);
+        bishopDirection(board, myPosition, piece, 1, -1, bishopValidMoves);
 
-        return bishop_valid_moves;
+        return bishopValidMoves;
 
 
     }
@@ -38,18 +38,18 @@ public class BishopMovesCalculator {
 
 
 
-    public void bishopDirection (ChessBoard board, ChessPosition myPosition, ChessPiece piece, int row, int col, Collection<ChessMove> bishop_valid_moves){
+    public void bishopDirection (ChessBoard board, ChessPosition myPosition, ChessPiece piece, int row, int col, Collection<ChessMove> bishopValidMoves){
         // initialize direction for while loop:
         ChessPosition position_next = new ChessPosition(myPosition.getRow() + row, myPosition.getColumn() + col);
         while ((position_next.getRow() != 9 && position_next.getRow() != 0) && (position_next.getColumn() != 9 && position_next.getColumn() != 0)) {
             ChessPiece piece_next = board.getPiece(new ChessPosition(position_next.getRow(), position_next.getColumn()));
             if (piece_next == null) {
                 // if space is empty, add move to list
-                bishop_valid_moves.add(new ChessMove(myPosition, position_next, null));
+                bishopValidMoves.add(new ChessMove(myPosition, position_next, null));
             } else {
                 // check enemy or friend, add to list if enemy:
                 if (piece.getTeamColor() != piece_next.getTeamColor()) {
-                    bishop_valid_moves.add(new ChessMove(myPosition, position_next, null));
+                    bishopValidMoves.add(new ChessMove(myPosition, position_next, null));
                 }
                 // break out of this direction loop bc space is blocked
                 break;
