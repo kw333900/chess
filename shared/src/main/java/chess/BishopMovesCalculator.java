43c241f49497a7df98f9bc1a350f18bd423c2072
kodym <kodymckaywastlund@outlook.com>
2026-07-15 20:30:49 -0600

    fixing code quality (fixed bishop movement)

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -19,96 +19,16 @@ public class BishopMovesCalculator {
         }
 
         // UP_RIGHT direction (row+1, col+1):
-
-
-        // initialize direction for while loop:
-        ChessPosition position_next = new ChessPosition(myPosition.getRow() + 1, myPosition.getColumn() + 1);
-        while ((position_next.getRow() != 9 && position_next.getRow() != 0) && (position_next.getColumn() != 9 && position_next.getColumn() != 0)) {
-            ChessPiece piece_next = board.getPiece(new ChessPosition(position_next.getRow(), position_next.getColumn()));
-            if (piece_next == null) {
-                // if space is empty, add move to list
-                bishop_valid_moves.add(new ChessMove(myPosition, position_next, null));
-            } else {
-                // check enemy or friend, add to list if enemy:
-                if (piece.getTeamColor() != piece_next.getTeamColor()) {
-                    bishop_valid_moves.add(new ChessMove(myPosition, position_next, null));
-                }
-                // break out of this direction loop bc space is blocked
-                break;
-            }
-            // increment to next position in that direction
-            position_next = new ChessPosition(position_next.getRow() + 1, position_next.getColumn() + 1);
-        }
-
+        bishopDirection(board, myPosition, piece, 1, 1, bishop_valid_moves);
 
         // DOWN_RIGHT direction (row-1, col+1):
-
-
-        // initialize direction for while loop:
-        position_next = new ChessPosition(myPosition.getRow() - 1, myPosition.getColumn() + 1);
-        while ((position_next.getRow() != 9 && position_next.getRow() != 0) && (position_next.getColumn() != 9 && position_next.getColumn() != 0)) {
-            ChessPiece piece_next = board.getPiece(new ChessPosition(position_next.getRow(), position_next.getColumn()));
-            if (piece_next == null) {
-                // if space is empty, add move to list
-                bishop_valid_moves.add(new ChessMove(myPosition, position_next, null));
-            } else {
-                // check enemy or friend, add to list if enemy:
-                if (piece.getTeamColor() != piece_next.getTeamColor()) {
-                    bishop_valid_moves.add(new ChessMove(myPosition, position_next, null));
-                }
-                // break out of this direction loop bc space is blocked
-                break;
-            }
-            // increment to next position in that direction
-            position_next = new ChessPosition(position_next.getRow() - 1, position_next.getColumn() + 1);
-        }
-
+        bishopDirection(board, myPosition, piece, -1, 1, bishop_valid_moves);
 
         // DOWN_LEFT direction (row-1, col-1):
-
-
-        // initialize direction for while loop:
-        position_next = new ChessPosition(myPosition.getRow() - 1, myPosition.getColumn() - 1);
-        while ((position_next.getRow() != 9 && position_next.getRow() != 0) && (position_next.getColumn() != 9 && position_next.getColumn() != 0)) {
-            ChessPiece piece_next = board.getPiece(new ChessPosition(position_next.getRow(), position_next.getColumn()));
-            if (piece_next == null) {
-                // if space is empty, add move to list
-                bishop_valid_moves.add(new ChessMove(myPosition, position_next, null));
-            } else {
-                // check enemy or friend, add to list if enemy:
-                if (piece.getTeamColor() != piece_next.getTeamColor()) {
-                    bishop_valid_moves.add(new ChessMove(myPosition, position_next, null));
-                }
-                // break out of this direction loop bc space is blocked
-                break;
-            }
-            // increment to next position in that direction
-            position_next = new ChessPosition(position_next.getRow() - 1, position_next.getColumn() - 1);
-        }
-
+        bishopDirection(board, myPosition, piece, -1, -1, bishop_valid_moves);
 
         // UP_LEFT direction (row+1, col-1):
-
-
-        // initialize direction for while loop:
-        position_next = new ChessPosition(myPosition.getRow() + 1, myPosition.getColumn() - 1);
-        while ((position_next.getRow() != 9 && position_next.getRow() != 0) && (position_next.getColumn() != 9 && position_next.getColumn() != 0)) {
-            ChessPiece piece_next = board.getPiece(new ChessPosition(position_next.getRow(), position_next.getColumn()));
-            if (piece_next == null) {
-                // if space is empty, add move to list
-                bishop_valid_moves.add(new ChessMove(myPosition, position_next, null));
-            } else {
-                // check enemy or friend, add to list if enemy:
-                if (piece.getTeamColor() != piece_next.getTeamColor()) {
-                    bishop_valid_moves.add(new ChessMove(myPosition, position_next, null));
-                }
-                // break out of this direction loop bc space is blocked
-                break;
-            }
-            // increment to next position in that direction
-            position_next = new ChessPosition(position_next.getRow() + 1, position_next.getColumn() - 1);
-        }
-
+        bishopDirection(board, myPosition, piece, 1, -1, bishop_valid_moves);
 
         return bishop_valid_moves;
 
@@ -117,6 +37,27 @@ public class BishopMovesCalculator {
 
 
 
+
+    public void bishopDirection (ChessBoard board, ChessPosition myPosition, ChessPiece piece, int row, int col, Collection<ChessMove> bishop_valid_moves){
+        // initialize direction for while loop:
+        ChessPosition position_next = new ChessPosition(myPosition.getRow() + row, myPosition.getColumn() + col);
+        while ((position_next.getRow() != 9 && position_next.getRow() != 0) && (position_next.getColumn() != 9 && position_next.getColumn() != 0)) {
+            ChessPiece piece_next = board.getPiece(new ChessPosition(position_next.getRow(), position_next.getColumn()));
+            if (piece_next == null) {
+                // if space is empty, add move to list
+                bishop_valid_moves.add(new ChessMove(myPosition, position_next, null));
+            } else {
+                // check enemy or friend, add to list if enemy:
+                if (piece.getTeamColor() != piece_next.getTeamColor()) {
+                    bishop_valid_moves.add(new ChessMove(myPosition, position_next, null));
+                }
+                // break out of this direction loop bc space is blocked
+                break;
+            }
+            // increment to next position in that direction
+            position_next = new ChessPosition(position_next.getRow() + row, position_next.getColumn() + col);
+        }
+    }
 
 
 
