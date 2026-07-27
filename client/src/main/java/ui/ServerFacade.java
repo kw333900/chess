cfd20323dd4b2f4f65ad4f0c230133c7a01d7bf5
kodym <kodymckaywastlund@outlook.com>
2026-07-27 14:00:35 -0600

    created ServerFacade class in ui package and started writing ServerFacadeTests

---
+++ client/src/main/java/ui/ServerFacade.java
@@ -0,0 +1,8 @@
+package ui;
+
+public class ServerFacade {
+
+
+    public ServerFacade(int port) {
+    }
+}
--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -2,17 +2,20 @@ package client;
 
 import org.junit.jupiter.api.*;
 import server.Server;
+import ui.ServerFacade;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade(port);
     }
 
     @AfterAll
@@ -21,9 +24,28 @@ public class ServerFacadeTests {
     }
 
 
-    @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    /* From spec: Make sure you clear your database between each test.
+    You can do this in a method that has the @BeforeEach annotation. */
+    @BeforeEach
+    public void clearDatabase(){
+//        facade.clear();
+        // IDK if this is correct^
     }
+
+//    @Test
+//    public void sampleTest() {
+//        Assertions.assertTrue(true);
+//    }
+
+
+
+    // FROM SPEC:
+//    @Test
+//    void register() throws Exception {
+//        var authData = facade.register("player1", "password", "p1@email.com");
+//        assertTrue(authData.authToken().length() > 10);
+//    }
+
+
 
 }
