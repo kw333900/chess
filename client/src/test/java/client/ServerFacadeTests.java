6594de4909c21480da90e338c7709ac182e1f771
kodym <kodymckaywastlund@outlook.com>
2026-07-28 16:44:44 -0600

    put declarations for all endpoint methods in ServerFacade

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -14,8 +14,8 @@ public class ServerFacadeTests {
     public static void init() {
         server = new Server();
         var port = server.run(0);
-        System.out.println("Started test HTTP server on " + port);
-        facade = new ServerFacade(port);
+//        System.out.println("Started test HTTP server on " + port);
+//        facade = new ServerFacade(port);
     }
 
     @AfterAll
