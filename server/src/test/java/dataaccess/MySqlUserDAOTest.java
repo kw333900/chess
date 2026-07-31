ea8d0cadb67d0a0c47f0b6b0c126686ebc5bd331
kodym <kodymckaywastlund@outlook.com>
2026-07-30 18:32:03 -0600

    ResponseException fix

--- server/src/test/java/dataaccess/MySqlUserDAOTest.java
+++ server/src/test/java/dataaccess/MySqlUserDAOTest.java
@@ -5,7 +5,6 @@ import model.UserData;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import service.UserService;
 
 import static org.junit.jupiter.api.Assertions.*;
 
